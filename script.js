c24aa8842cc26e1abc8bc01e7fbe33bd533f575f
zlatakjshkarova <147971125+zlatakjshkarova@users.noreply.github.com>
2024-11-15 22:17:43 +0500

    Add files via upload

---
+++ script.js
@@ -0,0 +1,65 @@
+function hello1(user, msg) {
+    let relt = `${user, msg}`;
+    return relt
+
+}
+hello1("Привет,JavaScript");
+
+
+function red(der, far) {
+
+    if (der > 2 && far > 2) {
+        return der * 2 && far * 2
+    } else if (der < 3 && far < 3) {
+        return der + 3 && far + 3
+    } else if (der > 2 && far < 3) {
+        return der, far
+    }
+
+}
+
+let der, far = red(2, 7);
+console.log(der, far)
+
+
+
+function checkVelo(wheels, weight) {
+
+
+    if (wheels < 2 && weight < 100) {
+
+        return "Парковка разрешена"
+    } else {
+        return "Парковка только для велосипедов"
+    }
+}
+let wheels, weight = checkVelo(2, 102);
+console.log(wheels, weight);
+
+
+
+
+
+
+function func(param1, param2, param3) {
+
+    console.log(param1 + param2 + param3)
+
+}
+func(1, 2, 3)
+
+
+
+
+function rame(as) {
+    let wer = as ** 2
+    return wer
+}
+rame(3)
+function nwer(res) {
+    let waq = res ** 3
+    console.log(waq)
+}
+nwer(4);
+
+
